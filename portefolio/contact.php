afd4545c1367f6489f496098bede0ba1c2fc6cd6
kayyhan <cuvillier.remy@hotmail.fr>
2022-07-27 21:18:02 +0200

    install composer

--- portefolio/contact.php
+++ portefolio/contact.php
@@ -1,5 +1,51 @@
 <?php
 
+$from = $_POST['email']; //mail de la personne
+$topic = $_POST['objet']; //objet
+$text = $_POST['message']; // message
+$name = $_POST['name']; // nom de la personne
 
-if (isset($_POST['message'])) {
+
+$secret = 'q6muPLOjMCmH3DXC73lh'; //mot de passe de la boite mail
+
+//biblio phpmailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP //Service
+    $mail->Host       = 'ssl0.ovh.net';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication 
+    $mail->Username   = 'xilitest@hiroshimc.net';                     //SMTP username
+    $mail->Password   = $secret;                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($from, $name);
+    $mail->addAddress('xilitest@hiroshimc.net', 'Cuvillier Rémy');
+
+    //Content
+    $mail->isHTML(true); //ajouter du html dans le message
+    $mail->Subject = $topic;
+    $mail->Body    = $text;
+    $mail->AltBody = $text;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
+header('Location: index.html');
---
+++ portefolio/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '417ef1e6362b1c3460e3c9ccf699e1cb1750c4d5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '417ef1e6362b1c3460e3c9ccf699e1cb1750c4d5',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.3',
+            'version' => '6.6.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => '9400f305a898f194caff5521f64e5dfa926626f3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
